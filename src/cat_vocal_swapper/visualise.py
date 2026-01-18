34d67e8fd737532afab8294a9f0d52095c7f191b
Zac Faulks <92555610+Toggleableable@users.noreply.github.com>
2026-01-18 22:22:58 +0000

    Removed old visualisation code, replaced with built in librosa version

--- src/cat_vocal_swapper/visualise.py
+++ src/cat_vocal_swapper/visualise.py
@@ -6,19 +6,6 @@ from numpy.typing import NDArray
 # pyright: reportUnknownMemberType=false, reportUnusedCallResult=false
 
 
-def old_visualise(samples: NDArray[np.float32]) -> None:
-    plt.figure(figsize=(15, 4))
-    plt.plot(samples, color="slateblue")
-    # plt.xlim(0, 200000)
-    # plt.ylim(-25000, 25000)
-    plt.title("Original Audio Waveform")
-    plt.xlabel("Sample Index")
-    plt.ylabel("Amplitude")
-    plt.grid(True)
-    plt.tight_layout()
-    plt.show()
-
-
 def visualise(samples: NDArray[np.float32], sample_rate: float) -> None:
     plt.figure(figsize=(15, 4))
     waveshow(samples, sr=sample_rate)
