55e4fee076d68c9bcf7b08ed6e831ca897821142
Zac Faulks <92555610+Toggleableable@users.noreply.github.com>
2026-01-18 22:45:09 +0000

    Moved split_audio_by_onsets function to audio functions file

--- src/cat_vocal_swapper/audio_functions.py
+++ src/cat_vocal_swapper/audio_functions.py
@@ -6,7 +6,9 @@ from numpy.typing import NDArray
 
 
 def load_audio(path: str) -> tuple[NDArray[np.float32], float]:
-    return librosa.load(path, sr=None, mono=True)
+    audio, sample_rate = librosa.load(path, sr=None, mono=True)
+    audio, _ = librosa.effects.trim(audio)  # pyright: ignore[reportUnknownMemberType]
+    return audio, sample_rate
 
 
 def play(
@@ -17,3 +19,19 @@ def play(
 
 def export(audio: NDArray[np.float32], sample_rate: float) -> None:
     sf.write("output.mp3", audio, sample_rate)  # pyright: ignore[reportUnknownMemberType]
+
+
+def split_audio_by_onsets(
+    audio: NDArray[np.float32],
+    sample_rate: float,
+    backtrack: bool = True,
+) -> list[NDArray[np.float32]]:
+    onset_samples = librosa.onset.onset_detect(
+        y=audio, sr=sample_rate, backtrack=backtrack, units="samples"
+    )
+
+    segments: list[NDArray[np.float32]] = []
+    for index in range(len(onset_samples) - 1):
+        segments.append(audio[onset_samples[index] : onset_samples[index + 1]])
+
+    return segments
--- src/cat_vocal_swapper/audio_test.py
+++ src/cat_vocal_swapper/audio_test.py
@@ -1,8 +1,4 @@
-import librosa
-import numpy as np
-from numpy.typing import NDArray
-
-from .audio_functions import export, load_audio, play
+from .audio_functions import export, load_audio, play, split_audio_by_onsets
 from .visualise import visualise
 
 
@@ -11,24 +7,8 @@ def test() -> None:
     audio, sample_rate = load_audio(path)
     segments = split_audio_by_onsets(audio, sample_rate)
 
-    export(segments[0], sample_rate)
+    # export(segments[0], sample_rate)
 
     for i in segments:
         play(i, sample_rate)
         visualise(i, sample_rate)
-
-
-def split_audio_by_onsets(
-    audio: NDArray[np.float32],
-    sample_rate: float,
-    backtrack: bool = True,
-) -> list[NDArray[np.float32]]:
-    onset_samples = librosa.onset.onset_detect(
-        y=audio, sr=sample_rate, backtrack=backtrack, units="samples"
-    )
-
-    segments: list[NDArray[np.float32]] = []
-    for index in range(len(onset_samples) - 1):
-        segments.append(audio[onset_samples[index] : onset_samples[index + 1]])
-
-    return segments
